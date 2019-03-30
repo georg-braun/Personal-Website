9575a7412a606207869a5f7bdf59de1bdbe33ced
Georg Braun <mail@georg-braun.de>
2019-03-30 18:28:11 +0100

    Added ejs as view engine

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const path = require('path');
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => { res.send("Hey") });
+app.use(express.static(path.join(__filename, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => { res.render("pages/home") });
 
 app.listen(PORT, () => { console.log(`Server started on Port ${PORT}`)});
