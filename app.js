403968e61944d4caf57c06ebeedd6e269dc8ed9b
Georg Braun <Georg.Braun92@gmail.com>
2019-06-04 17:52:05 +0200

    Stammbaum über DTree rendern. Noch mit Beispieldaten LICENSE ergänzt

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require('path');
+const noSniff = require('dont-sniff-mimetype')
+const dTree = require("d3-dtree");
+const d3 = require("d3");
 
 const app = express();
 
@@ -9,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 var scssPath = path.join(__dirname, 'public/stylesheets/timeline.scss')
 console.log(scssPath);
 
-
+app.use(noSniff());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -23,5 +26,9 @@ app.get("/projects", (req, res) => { res.render("pages/projects") });
 app.get("/profile", (req, res) => { res.render("pages/profile") });
 
 app.get("/impressum", (req, res) => {res.render("pages/impressum") });
+
+app.get("/stammbaum", (req, res) => {
+    res.render("pages/stammbaum", { StammbaumData : FStammbaumData })    
+});
 
 app.listen(PORT, () => { console.log(`Server started on Port ${PORT}`)});
