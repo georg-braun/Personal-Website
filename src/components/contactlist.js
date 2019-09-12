ff8b613347b60198a1672ba7962cc36faf760593
Georg Braun <Georg.Braun92@gmail.com>
2019-09-12 18:33:04 +0200

    Navbar weiter gestaltet

--- src/components/contactlist.js
+++ src/components/contactlist.js
@@ -10,31 +10,23 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 const Contactlist = () => {
   return (
-    <Nav>
-      <Nav.Item as="li">
-        <a href="https://www.xing.com/profile/Georg_Braun18" target="_blank">
-          <FontAwesomeIcon icon={faXing} />
-        </a>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <a
-          href="https://www.linkedin.com/in/georg-braun-41286b140/"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </a>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <a href="https://gitlab.com/georg.braun92" target="_blank">
-          <FontAwesomeIcon icon={faGitlab} />
-        </a>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <a href="mailto:mail@georg-braun.de" target="_blank">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-      </Nav.Item>
-    </Nav>
+    <span>
+      <a href="https://www.xing.com/profile/Georg_Braun18" target="_blank">
+        <FontAwesomeIcon icon={faXing} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/georg-braun-41286b140/"
+        target="_blank"
+      >
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </a>
+      <a href="https://gitlab.com/georg.braun92" target="_blank">
+        <FontAwesomeIcon icon={faGitlab} />
+      </a>
+      <a href="mailto:mail@georg-braun.de" target="_blank">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+    </span>
   )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,27 @@ import React from "react"
 import VisitCard from "./visitcard"
 
 import Contactlist from "./contactlist"
+import { Navbar, Nav } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
+  <Navbar bg="dark" expand="lg">
+    <Navbar.Brand>Georg Braun</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link>Blog</Nav.Link>
+        <Nav.Link>Knowledge</Nav.Link>
+        <Nav.Link>Profil</Nav.Link>
+      </Nav>
+      <Contactlist></Contactlist>
+    </Navbar.Collapse>
+  </Navbar>
+  /*
   <header>
     <VisitCard></VisitCard>
     <Contactlist></Contactlist>
   </header>
+  */
 )
 
 Header.propTypes = {
