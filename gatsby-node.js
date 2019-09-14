a69c993e3d4f029a3bf8113c5aac062fa00d2256
Georg Braun <Georg.Braun92@gmail.com>
2019-09-14 13:54:43 +0200

    Verzicht auf Unterordner

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `it` })
+    const slug = createFilePath({ node, getNode, basePath: `` })
     createNodeField({
       node,
       name: `slug`,
