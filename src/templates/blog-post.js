1e97c08bbccd37475df4d21cbd6be0a01e77e7c2
Georg Braun <Georg.Braun92@gmail.com>
2019-09-19 20:27:56 +0200

    Markdown Styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styled from "styled-components"
-
+import "./markdown-post.css"
 
 const MetadataEntry = styled.span`
   margin-right: 20px;
