2aefad333ece80ea2b986c358f28c209b129760a
Georg Braun <Georg.Braun92@gmail.com>
2019-09-25 15:56:13 +0200

    Suche case insensitive

--- src/pages/wissen.js
+++ src/pages/wissen.js
@@ -2,31 +2,34 @@ import React, { useState } from "react"
 import { graphql } from "gatsby"
 import { Alert } from "react-bootstrap"
 import styled from "styled-components"
-import { Input } from 'semantic-ui-react'
+import { Input } from "semantic-ui-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import KnowledgeLink from "../components/knowledge-link"
 import Contactlist from "../components/contactlist"
 
-
-
 // Assumes that the data is ordered by category
 function createKnowledgeOverview(data, filterString) {
   var hLastCategory = null
+  var hFilterString = filterString.toLowerCase()
+
+  var hFilterRegExp = new RegExp(filterString, "i") // i = case insensitive
 
   var hOverview = data.allMarkdownRemark.edges.map(({ node }) => {
-    
-    if (node.frontmatter.title.includes(filterString) || node.rawMarkdownBody.includes(filterString) || node.frontmatter.tags.includes(filterString)) {
-
+    if (
+      hFilterRegExp.test(node.frontmatter.title) ||
+      hFilterRegExp.test(node.rawMarkdownBody) ||
+      hFilterRegExp.test(node.frontmatter.tags)
+    ) {
       var hCurrentCategory = node.frontmatter.category
-  
+
       // Check if a new category header is necessary
       var hCategoryHeader = null
       if (hCurrentCategory !== hLastCategory) {
         hCategoryHeader = <h3>{hCurrentCategory}</h3>
         hLastCategory = hCurrentCategory
       }
-  
+
       return (
         <>
           {hCategoryHeader}
@@ -43,15 +46,12 @@ function createKnowledgeOverview(data, filterString) {
   return hOverview
 }
 
-
 const StyledAlert = styled(Alert)`
   background-color: #1b1c1d;
   color: white;
 `
 
-
-
-const siteTitle = "Wissen";
+const siteTitle = "Wissen"
 
 export default ({ data }) => {
   const [show, setShow] = useState(true)
@@ -60,11 +60,14 @@ export default ({ data }) => {
   var hAlert = null
   if (show) {
     hAlert = (
-      <StyledAlert  onClose={() => setShow(false)} dismissible>
+      <StyledAlert onClose={() => setShow(false)} dismissible>
         <Alert.Heading>Was ist das hier?</Alert.Heading>
         <p>
-          Das hier ist meine <span role="img" aria-label="sheet">📝</span>  Notiz-Sammlung zu diversen Themen, die mir als
-          Nachschlagewerk dient.{" "}
+          Das hier ist meine{" "}
+          <span role="img" aria-label="sheet">
+            📝
+          </span>{" "}
+          Notiz-Sammlung zu diversen Themen, die mir als Nachschlagewerk dient.{" "}
         </p>
         <p>
           Bereits im Studium habe ich nichts davon gehalten Wissen für mich zu
@@ -73,31 +76,36 @@ export default ({ data }) => {
           profitieren können.
         </p>
         <p>
-        <span role="img" aria-label="warning">⚠️</span> Sehr wahrscheinlich sind nicht alle Notiz-Inhalte korrekt.{" "}
-          <br></br>
-          <span role="img" aria-label="wave">🙋</span> Feedback ist immer gerne gesehen: <strong>@</strong> ( <Contactlist></Contactlist> )
+          <span role="img" aria-label="warning">
+            ⚠️
+          </span>{" "}
+          Sehr wahrscheinlich sind nicht alle Notiz-Inhalte korrekt. <br></br>
+          <span role="img" aria-label="wave">
+            🙋
+          </span>{" "}
+          Feedback ist immer gerne gesehen: <strong>@</strong> ({" "}
+          <Contactlist></Contactlist> )
         </p>
       </StyledAlert>
     )
-  } 
-
-
-
+  }
 
   return (
-    <Layout title={siteTitle} >
-      <SEO title={siteTitle}  />
+    <Layout title={siteTitle}>
+      <SEO title={siteTitle} />
       {hAlert}
       <h6>{data.allMarkdownRemark.totalCount} Themen</h6>
-      <Input laceholder="Titel, Inhalt, Tag" onChange={(event, data) => setFilter(data.value)}
-          action={{
-            color: 'teal',
-            labelPosition: 'left',
-            icon: 'search',
-            content: 'Suche',
-          }}
-          actionPosition='left'
-          placeholder='Titel, Inhalt, Tag'
+      <Input
+        laceholder="Titel, Inhalt, Tag"
+        onChange={(event, data) => setFilter(data.value)}
+        action={{
+          color: "teal",
+          labelPosition: "left",
+          icon: "search",
+          content: "Suche",
+        }}
+        actionPosition="left"
+        placeholder="Titel, Inhalt, Tag"
       ></Input>
 
       {createKnowledgeOverview(data, filter)}
