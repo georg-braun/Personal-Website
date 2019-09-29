45b8db7277aa41daf60bdc308e382d3d4260df53
Georg Braun <Georg.Braun92@gmail.com>
2019-09-29 10:11:59 +0200

    Projektseite etwas aufgehübscht

--- src/pages/projekte.js
+++ src/pages/projekte.js
@@ -2,19 +2,22 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Row, Col, Badge } from "react-bootstrap"
+import { Button } from "semantic-ui-react"
 import Img from "gatsby-image"
 import { graphql } from "gatsby"
 import styled from "styled-components"
-import { FaClock } from "react-icons/fa"
+import { FaClock, FaGitlab, FaReact } from "react-icons/fa"
 
 const StyledProjectContainer = styled.div`
-  :hover {
-    transform: translate(0, -5px);
-    -webkit-transform: translate(0, -5px);
-    -o-transform: translate(0, -5px);
-    -moz-transform: translate(0, -5px);
-    transition-duration: 0.5s;
-  }
+  border-style: solid;
+  padding-bottom: 10px;
+  border-width: 1px;
+  border-color: lightgrey;
+  border-radius: 5px;
+`
+
+const StyledButton = styled(Button)`
+  margin-bottom: 5px !important;
 `
 
 const ProjectContainer = styled.div`
@@ -26,16 +29,20 @@ const ProjectTitle = styled.h3`
 `
 
 const ProjectDuration = styled.span`
-color: gray;
+  color: gray;
+`
+
+const ProjectBody = styled.div`
+  margin: 10px;
 `
 
 const ProjectDescription = styled.div`
-margin-top: 10px;
+  margin-top: 10px;
 `
 
 const StyledBadge = styled(Badge)`
-margin-right: 5px;
-font-size: 0.9em;
+  margin-right: 5px;
+  font-size: 0.9em;
 `
 
 const siteTitle = "Projekte"
@@ -46,23 +53,37 @@ export default ({ data }) => (
     <ProjectContainer>
       <Row>
         <Col lg="6">
-          <a
-            href="http://stammbaum.georg-braun.de"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledProjectContainer>
+          <StyledProjectContainer>
+            <a
+              href="http://stammbaum.georg-braun.de"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Img fluid={data.allImageSharp.edges[0].node.fluid} alt="" />
+              <ProjectBody>
+
+
+
               <ProjectTitle>Familienstammbaum</ProjectTitle>
+
+              <StyledButton
+                floated="right"
+                href="https://gitlab.com/georg.braun92/family-tree/blob/master/README.md"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGitlab /> Code + Doku
+              </StyledButton>
               <ProjectDuration>
-              <FaClock /> 08/2019
-                </ProjectDuration>
-                <ProjectDescription>
-                <StyledBadge variant="primary">React</StyledBadge> 
+                <FaClock /> 08/2019
+              </ProjectDuration>
+              <ProjectDescription>
+                <StyledBadge variant="primary"><FaReact /> React</StyledBadge>
                 <StyledBadge variant="primary">dTree</StyledBadge>
-                </ProjectDescription>
-            </StyledProjectContainer>
-          </a>
+              </ProjectDescription>
+              </ProjectBody>
+            </a>
+          </StyledProjectContainer>
         </Col>
       </Row>
     </ProjectContainer>
