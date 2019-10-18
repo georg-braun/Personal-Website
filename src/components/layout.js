228cbf68cdcbd69d82ff49068225d42d5a856135
Georg Braun <Georg.Braun92@gmail.com>
2019-10-18 23:54:06 +0200

    Am Responsive Design weiter gearbeitet

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,22 +19,32 @@ import {
   Grid,
   Sticky,
   Table,
+  Sidebar,
 } from "semantic-ui-react"
 import { FaXing, FaLinkedin, FaGitlab, FaEnvelope } from "react-icons/fa"
 import AvatarImg from "../images/avatar.png"
 
-const StyledContainer = styled(Container)`
-  padding-top: 40px;
-`
+
+const cMobileDesktopViewWidthBreakPoint = "600px";
+
+
 
 
 const ProfileContainer = styled.div`
   padding-top: 50px;
 
-  @media (max-width: 600px) {
+  @media (max-width: 991px) {
     display: none !important;
   }
 `
+
+const ComputerViewMenu = styled(Menu.Menu)`
+  @media (max-width: ${cMobileDesktopViewWidthBreakPoint}) {
+    display: none !important;
+  }
+`
+
+
 
 const ImageContainer = styled.div`
   max-width: 120px;
@@ -55,6 +65,12 @@ const FixedContent = styled.div`
   position: fixed;
 `
 
+const StyledBurgerMenu = styled(Menu.Item)`
+@media (min-width: ${cMobileDesktopViewWidthBreakPoint}) {
+  display: none !important;
+}
+`
+
 const PersonInfo = ({ children, link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
@@ -63,71 +79,109 @@ const PersonInfo = ({ children, link }) => {
   )
 }
 
+const MenuEntries = () => {
+    
+  return (
+    <>
+      <Menu.Item as="a" href="/">
+        <Icon name="home" />
+        Blog
+      </Menu.Item>
+      <Menu.Item as="a" href="/wissen">
+        <Icon name="book" />
+        Wissen
+      </Menu.Item>
+      <Menu.Item as="a" href="/projekte">
+        <Icon name="pencil" />
+        Projekte
+      </Menu.Item>
+      <Menu.Item as="a" href="/profil">
+        <Icon name="user circle" />
+        Profil
+      </Menu.Item>
+    </>
+  )
+}
+
 // Um die Kontext-Referenz für das Sticky zu erstellen ist es notwendig eine Komponenten Klasse zu erstellen
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+  const [sidebarIsVisible, setVisible] = useState(false)
+  return (
+    <>
+      <Sidebar
+        className="mobile-sidebar"
+        as={Menu}
+        animation="overlay"
+        icon="labeled"
+        onHide={() => setVisible(false)}
+        vertical
+        visible={sidebarIsVisible}
+        width="thin"
+      >
+        <MenuEntries />
+      </Sidebar>
 
-    return (
-      <>
-      
-        <Container>
-          <StyledMenu size="huge" borderless>
-            <Menu.Menu position="right">
-              <Menu.Item as="a" href="/">
-                <Icon name="home" />
-                Blog
-              </Menu.Item>
-              <Menu.Item as="a" href="/wissen">
-                <Icon name="book" />
-                Wissen
-              </Menu.Item>
-              <Menu.Item as="a" href="/projekte">
-                <Icon name="pencil" />
-                Projekte
-              </Menu.Item>
-              <Menu.Item as="a" href="/profil">
-                <Icon name="user circle" />
-                Profil
-              </Menu.Item>
-            </Menu.Menu>
-          </StyledMenu>
+      <Sidebar.Pusher></Sidebar.Pusher>
 
-          <Grid>
-            <Grid.Column computer={4}>
-              
-              <ProfileContainer>
-                <FixedContent>
-                  <ImageContainer>
-                    <Image src={AvatarImg} circular></Image>
-                  </ImageContainer>
-
-                  <Header>Georg Braun</Header>
-                  <p>Softwareentwickler</p>
-
-                  <PersonInfo text="Aachen" />
-                  <PersonInfo link="https://www.xing.com/profile/Georg_Braun18">
-                    <FaXing /> Xing
-                  </PersonInfo>
-                  <PersonInfo link="https://www.linkedin.com/in/georg-braun-41286b140/">
-                    <FaLinkedin /> Linkedin
-                  </PersonInfo>
-
-                  <PersonInfo link="https://gitlab.com/georg.braun92">
-                    <FaGitlab /> Gitlab
-                  </PersonInfo>
-                  <PersonInfo link="mailto:mail@georg-braun.de">
-                    <FaEnvelope /> Mail
-                  </PersonInfo>
-                </FixedContent>
-              </ProfileContainer>
-            </Grid.Column>
-            <Grid.Column mobile={16} computer={12}>{children}</Grid.Column>
-          </Grid>
-        </Container>
-      </>
-    )
-  }
+      <Container>
+        <StyledMenu size="huge" borderless >
+        <StyledBurgerMenu>
+              <div
+                onClick={() => {
+                  setVisible(true)
+                }}
+              >
+                <Icon name="bars"></Icon>
+              </div>
+            </StyledBurgerMenu>
+          <ComputerViewMenu position="right">
+            
 
 
+            <MenuEntries />
+
+
+            
+          </ComputerViewMenu>
+        </StyledMenu>
+
+        <Grid>
+          <Grid.Column computer={4}>
+            <ProfileContainer>
+              <FixedContent>
+                <ImageContainer>
+                  <Image src={AvatarImg} circular></Image>
+                </ImageContainer>
+
+                <Header>Georg Braun</Header>
+                <p>Softwareentwickler</p>
+
+                <PersonInfo text="Aachen" />
+                <PersonInfo link="https://www.xing.com/profile/Georg_Braun18">
+                  <FaXing /> Xing
+                </PersonInfo>
+                <PersonInfo link="https://www.linkedin.com/in/georg-braun-41286b140/">
+                  <FaLinkedin /> Linkedin
+                </PersonInfo>
+
+                <PersonInfo link="https://gitlab.com/georg.braun92">
+                  <FaGitlab /> Gitlab
+                </PersonInfo>
+                <PersonInfo link="mailto:mail@georg-braun.de">
+                  <FaEnvelope /> Mail
+                </PersonInfo>
+              </FixedContent>
+            </ProfileContainer>
+          </Grid.Column>
+          <Grid.Column mobile={16} computer={12}>
+            {children}
+          </Grid.Column>
+        </Grid>
+        <Footer/>
+      </Container>
+    </>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
