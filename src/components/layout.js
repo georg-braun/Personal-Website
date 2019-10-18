5f2526d53b56fefdff8792b349d81c4b832d5865
Georg Braun <Georg.Braun92@gmail.com>
2019-10-18 20:58:37 +0200

    Erste Schritte in ein saubereres Design

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,125 +5,127 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useState } from "react"
+import React, { useState, Component } from "react"
 import PropTypes from "prop-types"
-import { Container } from "react-bootstrap"
 import styled from "styled-components"
 import ContactList from "./contactlist"
 import Footer from "./footer"
-import { Icon, Image, Menu, Segment, Sidebar } from "semantic-ui-react"
+import {
+  Container,
+  Icon,
+  Image,
+  Menu,
+  Header,
+  Grid,
+  Sticky,
+  Table,
+} from "semantic-ui-react"
+import { FaXing, FaLinkedin, FaGitlab, FaEnvelope } from "react-icons/fa"
 import AvatarImg from "../images/avatar.png"
 
 const StyledContainer = styled(Container)`
   padding-top: 40px;
 `
 
-const NameInfoItem = styled(Menu.Item)`
+const OnlyComputerColumn = styled(Grid.Column)`
   @media (max-width: 600px) {
     display: none !important;
   }
 `
-const AvatarName = styled.div`
-  margin-bottom: 12px;
-  font-size: 1.3em;
-`
-const AvatarDescription = styled.div`
-  margin-bottom: 9px;
-  font-size: 0.8em;
+const ProfileContainer = styled.div`
+  padding-top: 120px;
 `
 
-const Layout = ({ children, title }) => {
-  const [visible, setVisible] = useState(false)
+const ImageContainer = styled.div`
+  max-width: 120px;
+`
 
-  var SiteTitleInfo = null
-  if (title !== undefined) {
-    SiteTitleInfo = <Menu.Item>{title}</Menu.Item>
-  }
+const StyledMenu = styled(Menu)`
+  margin-top: 20px !important;
+  border: none !important;
+  box-shadow: none !important;
+`
 
+const StyledPersonInfoContent = styled.div`
+  margin-bottom: 10px;
+  color: black;
+`
+
+
+
+const PersonInfo = ({ children, link }) => {
   return (
-    <>
-      <Menu inverted fixed="top">
-        <div
-          onMouseOver={() => {
-            setVisible(true)
-          }}
-        >
-          <Menu.Item
-            onClick={() => {
-              setVisible(true)
-            }}
-          >
-            <Icon name="bars"></Icon>
-          </Menu.Item>
-        </div>
-
-        {SiteTitleInfo}
-        <Menu.Menu position="right">
-          <NameInfoItem position="right">Georg Braun</NameInfoItem>
-          <Menu.Item position="right">
-            <ContactList></ContactList>
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-
-      <Sidebar
-        className="mobile-sidebar"
-        as={Menu}
-        animation="overlay"
-        icon="labeled"
-        inverted
-        onHide={() => setVisible(false)}
-        vertical
-        visible={visible}
-        width="thin"
-      >
-        <Menu.Item>
-          <div>
-            <Image src={AvatarImg} circular></Image>
-            <AvatarName>Georg Braun</AvatarName>
-            <AvatarDescription>
-              <span role="img" aria-label="gps">
-                📍
-              </span>{" "}
-              Aachen
-            </AvatarDescription>
-            <AvatarDescription>
-              <span role="img" aria-label="worker">
-                👷
-              </span>{" "}
-              Softwareentwickler
-            </AvatarDescription>
-          </div>
-        </Menu.Item>
-
-        <Menu.Item as="a" href="/">
-          <Icon name="home" />
-          Blog
-        </Menu.Item>
-        <Menu.Item as="a" href="/wissen">
-          <Icon name="book" />
-          Wissen
-        </Menu.Item>
-        <Menu.Item as="a" href="/projekte">
-          <Icon name="pencil" />
-          Projekte
-        </Menu.Item>
-        <Menu.Item as="a" href="/profil">
-          <Icon name="user circle" />
-          Profil
-        </Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher>
-        <Segment basic>
-          <StyledContainer>
-            <main>{children}</main>
-          </StyledContainer>
-          <Footer>Hey</Footer>
-        </Segment>
-      </Sidebar.Pusher>
-    </>
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <StyledPersonInfoContent>{children}</StyledPersonInfoContent>
+    </a>
   )
+}
+
+
+
+// Um die Kontext-Referenz für das Sticky zu erstellen ist es notwendig eine Komponenten Klasse zu erstellen
+class Layout extends Component {
+  contextRef = React.createRef()
+
+  render() {
+    return (
+      <>
+        <Container>
+          <StyledMenu size="huge" borderless>
+            <Menu.Menu position="right">
+              <Menu.Item as="a" href="/">
+                <Icon name="home" />
+                Blog
+              </Menu.Item>
+              <Menu.Item as="a" href="/wissen">
+                <Icon name="book" />
+                Wissen
+              </Menu.Item>
+              <Menu.Item as="a" href="/projekte">
+                <Icon name="pencil" />
+                Projekte
+              </Menu.Item>
+              <Menu.Item as="a" href="/profil">
+                <Icon name="user circle" />
+                Profil
+              </Menu.Item>
+            </Menu.Menu>
+          </StyledMenu>
+
+          <Grid>
+            <Grid.Column  computer={4}>
+              <Sticky context={this.contextRef}>
+              <ProfileContainer>
+                  <ImageContainer>
+                    <Image src={AvatarImg} circular></Image>
+                  </ImageContainer>
+
+                  <Header>Georg Braun</Header>
+                  <p>Softwareentwickler</p>
+
+                  <PersonInfo text="Aachen" />
+                  <PersonInfo link="https://www.xing.com/profile/Georg_Braun18">
+                    <FaXing /> Xing
+                  </PersonInfo>
+                  <PersonInfo link="https://www.linkedin.com/in/georg-braun-41286b140/">
+                    <FaLinkedin /> Linkedin
+                  </PersonInfo>
+
+                  <PersonInfo link="https://gitlab.com/georg.braun92">
+                    <FaGitlab /> Gitlab
+                  </PersonInfo>
+                  <PersonInfo link="mailto:mail@georg-braun.de">
+                    <FaEnvelope /> Mail
+                  </PersonInfo>
+                </ProfileContainer>
+              </Sticky>
+            </Grid.Column>
+            <Grid.Column computer={12}>{this.props.children}</Grid.Column>
+          </Grid>
+        </Container>
+      </>
+    )
+  }
 }
 
 Layout.propTypes = {
