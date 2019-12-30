eee7937cd83191f72af6a80589b7e74f2216d96e
Georg Braun <mail@georg-braun.de>
2019-12-30 12:37:58 +0100

    Neue Startseite entwickeln die alle Posts anzeigt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
-
+import { Input } from "semantic-ui-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BlogEntry from "../components/blog-entry"
+import KnowledgeLink from "../components/knowledge-link"
+import styled from "styled-components"
 
 function createBlogOverview(_blogdata) {
   return _blogdata.edges.map(({ node }) => {
@@ -11,26 +13,125 @@ function createBlogOverview(_blogdata) {
     
   })
 }
-const siteTitle="Blog";
+const siteTitle="Home";
 
-export default ({ data }) => (
+const SearchContainer = styled.div`
+  position: fixed;
+  width: 300px;
+  right: 5vw;
+  
+`
 
-  <Layout>
-    <SEO title={siteTitle} />
-    {createBlogOverview(data.blogdata)}
-  </Layout>
-)
+const StyledInputContainer = styled.div`
+  width: 300px;
+  margin-bottom: 5px;
+  
+`
+
+
+const StyledTag = styled.span`
+  font-size: 0.8em;
+  margin-right: 5px;
+  display: inline-block;
+  text-decoration: underline;
+
+  :hover {
+    font-weight: bold;
+  }
+`
+
+var FTags;
+
+function createKnowledgeOverview(data, filterString) {
+  var hOverview = data.posts.edges.map(({ node }) => {
+
+    var hKnowledgeEntry = null;
+
+    if (
+      node.frontmatter.title.includes(filterString) ||
+      node.rawMarkdownBody.includes(filterString)
+    ) {
+      
+
+      hKnowledgeEntry = (
+        <>
+          <KnowledgeLink
+            title={node.frontmatter.title}
+            path={node.fields.slug}            
+          ></KnowledgeLink>
+        </>
+      )
+    }
+
+    return hKnowledgeEntry;
+   
+  })
+
+  return hOverview
+}
+
+function createTagContainer(tags){
+
+  
+  var hTags = tags.map((tag) => {
+    return (
+    <StyledTag onClick={() => {console.log("test")}}>
+      
+      {tag}
+    </StyledTag>
+      )
+  })
+
+  return hTags;
+  
+}
+
+
+
+export default ({ data }) => {
+  
+  const [filter, setFilter] = useState("")
+
+  return (
+    <Layout>
+      <SEO title={siteTitle} />
+      
+      <h6>{data.posts.totalCount} Themen</h6>
+      <SearchContainer>
+        <StyledInputContainer>
+        <Input
+          onChange={(event, data) => setFilter(data.value)}
+          action={{
+            color: "teal",
+            labelPosition: "left",
+            icon: "search",
+            content: "Suche",
+          }}
+          fluid
+          actionPosition="left"
+          placeholder="Titel, Inhalt"/>
+          </StyledInputContainer>
+          {createTagContainer(data.tags.distinct)}
+          
+      </SearchContainer>
+      
+
+      {createKnowledgeOverview(data, filter)}
+    </Layout>
+  )
+}
+
 
 export const query = graphql`
   {
-    blogdata: allMarkdownRemark(
+    posts: allMarkdownRemark(
       sort: {
         fields: [frontmatter___date]
         order: DESC
       }
       filter: {
         frontmatter: { category: { ne: null }, title: { ne: null } }
-        fileAbsolutePath: { regex: "/blog/" }
+        fileAbsolutePath: { regex: "/knowledge-repo/" }
       }
     ) {
       totalCount
@@ -43,16 +144,21 @@ export const query = graphql`
           frontmatter {
             title
             category
-            date(formatString: "DD MMMM YYYY", locale: "de-DE")
-            excerpt
-            image
+            tags
           }
           fileAbsolutePath
-          excerpt
-          timeToRead
+          tableOfContents
+          rawMarkdownBody
         }
       }
     }
+
+    tags:     
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/knowledge-repo/"}}) {
+        distinct(field: frontmatter___tags)
+      }
     
+    
+
   }
 `
--- src/pages/wissen.js
+++ src/pages/wissen.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react"
 import { graphql } from "gatsby"
-import { Alert } from "react-bootstrap"
 import styled from "styled-components"
 import { Input } from "semantic-ui-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import KnowledgeLink from "../components/knowledge-link"
-import Contactlist from "../components/contactlist"
 
 // Assumes that the data is ordered by category
 function createKnowledgeOverview(data, filterString) {
@@ -50,55 +48,17 @@ function createKnowledgeOverview(data, filterString) {
   return hOverview
 }
 
-const StyledAlert = styled(Alert)`
-  background-color: #1b1c1d;
-  color: white;
-`
-
 const siteTitle = "Wissen"
 
 export default ({ data }) => {
   const [show, setShow] = useState(false)
   const [filter, setFilter] = useState("")
 
-  // Wissen-Banner momentan deaktiviert
-  var hAlert = null
-  if (show) {
-    hAlert = (
-      <StyledAlert onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Was ist das hier?</Alert.Heading>
-        <p>
-          Das hier ist meine{" "}
-          <span role="img" aria-label="sheet">
-            📝
-          </span>{" "}
-          Notiz-Sammlung zu diversen Themen, die mir als Nachschlagewerk dient.{" "}
-        </p>
-        <p>
-          Bereits im Studium habe ich nichts davon gehalten Wissen für mich zu
-          behalten, weshalb ich auf diesem Wege meine Notizen öffentlich
-          zugänglich mache, sodass vielleicht auch weitere Personen davon
-          profitieren können.
-        </p>
-        <p>
-          <span role="img" aria-label="warning">
-            ⚠️
-          </span>{" "}
-          Sehr wahrscheinlich sind nicht alle Notiz-Inhalte korrekt. <br></br>
-          <span role="img" aria-label="wave">
-            🙋
-          </span>{" "}
-          Feedback ist immer gerne gesehen: <strong>@</strong> ({" "}
-          <Contactlist></Contactlist> )
-        </p>
-      </StyledAlert>
-    )
-  }
 
   return (
     <Layout>
       <SEO title={siteTitle} />
-      {hAlert}
+      
       <h6>{data.allMarkdownRemark.totalCount} Themen</h6>
       <Input
         onChange={(event, data) => setFilter(data.value)}
