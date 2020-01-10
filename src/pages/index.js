5e0c02b0986c68de3c1f72876eca9ee8810a7b11
Georg Braun <mail@georg-braun.de>
2020-01-10 18:58:58 +0100

    Datumswert aufsteigend

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,7 +155,7 @@ export const query = graphql`
     posts: allMarkdownRemark(
       sort: {
         fields: [frontmatter___date]
-        order: DESC
+        order: ASC
       }
       filter: {
         frontmatter: { title: { ne: null } }
