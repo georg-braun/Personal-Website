a9cd16b948bed654d7f2c11183e56c88e53150bb
Georg Braun <mail@georg-braun.de>
2020-12-27 19:53:49 +0100

    Überarbeitung der Software Projekt-Seite

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import BlogLink from "../components/blog-link"
 import styled from "styled-components"
 import dateFormat from "dateformat"
 
-const siteTitle = "Georg Braun"
+const siteTitle = "Startseite"
 
 const SearchContainer = styled.div`
   margin-bottom: 20px;
@@ -57,68 +57,8 @@ const StyledImg = styled(Img)`
   margin-top: 2px;
 `
 
-function frontmatterIsValid(edge) {
-  console.log(edge.node.frontmatter.title)
-  return (
-    edge.node.frontmatter.title !== null && edge.node.frontmatter.title !== ""
-  )
-}
-
-function createKnowledgeOverview(data, filterString) {
-  var hOverview = data.posts.edges
-    .filter(_ => frontmatterIsValid(_))
-    .map(({ node }) => {
-      if (
-        node.frontmatter.title.includes(filterString) ||
-        node.rawMarkdownBody.includes(filterString) ||
-        (node.frontmatter.tags != null &&
-          node.frontmatter.tags.includes(filterString))
-      ) {
-        var hBlogEntryDateString = ""
-        if (node.frontmatter.date != null) {
-          hBlogEntryDateString = dateFormat(
-            node.frontmatter.date,
-            "deutschKurz"
-          )
-        }
-
-        var hKnowledgeEntry = (
-          <>
-            <BlogLink
-              title={node.frontmatter.title}
-              path={node.fields.slug}
-              date={hBlogEntryDateString}
-              tags={node.frontmatter.tags}
-            ></BlogLink>
-          </>
-        )
-      }
-
-      return hKnowledgeEntry
-    })
-
-  return hOverview
-}
-
-function createTagContainer(tags, filterMethod) {
-  var hTags = tags.map(tag => {
-    return (
-      <StyledTag
-        onClick={() => {
-          filterMethod(tag)
-        }}
-      >
-        {tag}
-      </StyledTag>
-    )
-  })
-
-  return hTags
-}
 
 export default ({ data }) => {
-  const [filter, setFilter] = useState("")
-  dateFormat.masks.deutschKurz = "dd.mm.yyyy"
 
   return (
     <Layout>
@@ -153,70 +93,6 @@ export default ({ data }) => {
     </Layout>
   )
 }
-
-/*
-<h6>{data.posts.totalCount} Beiträge</h6>
-      <SearchContainer>
-        <StyledInputContainer>
-        <Input
-          onChange={(event, data) => setFilter(data.value)}
-          action={{
-            color: "teal",            
-            icon: "search",            
-          }}
-          fluid
-          value={filter}
-          actionPosition="left"          
-          placeholder="Titel, Inhalt, Tag"/>
-          </StyledInputContainer>
-          {createTagContainer(data.tags.distinct, setFilter)}          
-      </SearchContainer>
-
-
-      {createKnowledgeOverview(data, filter)}
-
-
-
-export const query = graphql`
-  {
-    posts: allMarkdownRemark(
-      sort: {
-        fields: [frontmatter___date]
-        order: DESC
-      }
-      filter: {
-        frontmatter: { title: { ne: null } }
-      }
-    ) {
-      totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-          id
-          frontmatter {
-            title
-            tags
-            date
-          }
-          fileAbsolutePath
-          tableOfContents
-          rawMarkdownBody
-        }
-      }
-    }
-
-    tags:     
-      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/knowledge-repo/"}}) {
-        distinct(field: frontmatter___tags)
-      }
-    
-    
-
-  }
-`
-*/
 
 export const query = graphql`
   {
---
+++ src/pages/software-artikel.js
@@ -0,0 +1,196 @@
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Input } from "semantic-ui-react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BlogLink from "../components/blog-link"
+import styled from "styled-components"
+import dateFormat from "dateformat"
+
+const siteTitle = "Startseite"
+
+const SearchContainer = styled.div`
+  margin-bottom: 20px;
+`
+
+const StyledInputContainer = styled.div`
+  width: 70vw;
+  margin-bottom: 10px;
+`
+
+const StyledTag = styled.span`
+  font-size: 0.8em;
+  margin-right: 5px;
+  display: inline-block;
+  text-decoration: underline;
+
+  :hover {
+    font-weight: bold;
+  }
+`
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: black;
+`
+
+const SiteFlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const WebsiteArea = styled.div`
+  width: 250px;
+  height: 200px;
+  margin: 10px;
+  padding: 10px;
+
+  border-radius: 5px;
+
+  text-align: center;
+`
+
+const StyledImg = styled(Img)`
+  max-height: 150px;
+  margin-top: 2px;
+`
+
+function frontmatterIsValid(edge) {
+  console.log(edge.node.frontmatter.title)
+  return (
+    edge.node.frontmatter.title !== null && edge.node.frontmatter.title !== ""
+  )
+}
+
+function createKnowledgeOverview(data, filterString) {
+  var hOverview = data.posts.edges
+    .filter(_ => frontmatterIsValid(_))
+    .map(({ node }) => {
+      if (
+        node.frontmatter.title.includes(filterString) ||
+        node.rawMarkdownBody.includes(filterString) ||
+        (node.frontmatter.tags != null &&
+          node.frontmatter.tags.includes(filterString))
+      ) {
+        var hBlogEntryDateString = ""
+        if (node.frontmatter.date != null) {
+          hBlogEntryDateString = dateFormat(
+            node.frontmatter.date,
+            "deutschKurz"
+          )
+        }
+
+        var hKnowledgeEntry = (
+          <>
+            <BlogLink
+              title={node.frontmatter.title}
+              path={node.fields.slug}
+              date={hBlogEntryDateString}
+              tags={node.frontmatter.tags}
+            ></BlogLink>
+          </>
+        )
+      }
+
+      return hKnowledgeEntry
+    })
+
+  return hOverview
+}
+
+function createTagContainer(tags, filterMethod) {
+  var hTags = tags.map(tag => {
+    return (
+      <StyledTag
+        onClick={() => {
+          filterMethod(tag)
+        }}
+      >
+        {tag}
+      </StyledTag>
+    )
+  })
+
+  return hTags
+}
+
+export default ({ data }) => {
+  const [filter, setFilter] = useState("")
+  dateFormat.masks.deutschKurz = "dd.mm.yyyy"
+
+  return (
+    <Layout>
+      <SEO title={siteTitle} />
+
+      <h6>{data.posts.totalCount} Beiträge</h6>
+      <SearchContainer>
+        <StyledInputContainer>
+        <Input
+          onChange={(event, data) => setFilter(data.value)}
+          action={{
+            color: "teal",            
+            icon: "search",            
+          }}
+          fluid
+          value={filter}
+          actionPosition="left"          
+          placeholder="Titel, Inhalt, Tag"/>
+          </StyledInputContainer>
+          {createTagContainer(data.tags.distinct, setFilter)}          
+      </SearchContainer>
+
+
+      {createKnowledgeOverview(data, filter)}
+
+    </Layout>
+  )
+}
+
+
+
+
+
+export const query = graphql`
+  {
+    posts: allMarkdownRemark(
+      sort: {
+        fields: [frontmatter___date]
+        order: DESC
+      }
+      filter: {
+        frontmatter: { title: { ne: null } }
+      }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          frontmatter {
+            title
+            tags
+            date
+          }
+          fileAbsolutePath
+          tableOfContents
+          rawMarkdownBody
+        }
+      }
+    }
+
+    tags:     
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/knowledge-repo/"}}) {
+        distinct(field: frontmatter___tags)
+      }
+    
+    
+
+  }
+`
+
+
+
