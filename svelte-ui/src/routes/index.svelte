a919e57360e932109939f2292f285275eb3435f0
georg-braun <Georg.Braun92@gmail.com>
2021-12-25 09:10:33 +0100

    show project

--- svelte-ui/src/routes/index.svelte
+++ svelte-ui/src/routes/index.svelte
@@ -1,1 +1,14 @@
-<p class="text-2xl text-center">🔨</p>
+<div>
+	<div
+		class="flex flex-wrap border-solid border-2 drop-shadow-xl border-gray-400 rounded-md  md:w-1/2"
+	>
+		<img src="garage-buddy-project.png" alt="Profile picture" />
+		<div class="p-2">
+			<p class="text-2xl font-semibold ">Garage buddy</p>
+			<p class="mt-2">
+				Neben dem Verwalten von Fahrzeug Inspektionen wird man auch an anstehende Inspektionen
+				erinnert
+			</p>
+		</div>
+	</div>
+</div>
