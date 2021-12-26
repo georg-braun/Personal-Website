1a526c9c8e72a274abd5b05297cdcb17d38b38fc
georg-braun <Georg.Braun92@gmail.com>
2021-12-26 21:03:29 +0100

    move project to own folder

---
+++ svelte-ui/src/components/project.svelte
@@ -0,0 +1,14 @@
+<div class="border-solid border-2 overflow-clip shadow-xl  rounded-md  md:w-1/3 m-4">
+	<div>
+		<img src="garage-buddy-project.png" alt="Profile picture" />
+		<div class="p-2">
+			<p class="text-2xl font-semibold ">Garage buddy</p>
+			<p class="mt-2">
+				Eine Webanwendung zum Verwalten von Fahrzeug-Inspektionen. Demnächst anstehende Inspektionen
+				werden hervorgehoben. Es war ein Hobby-Projekt um mehr über React, Next.js und Firebase zu
+				lernen.
+				<!-- Link zum Blog-Post -->
+			</p>
+		</div>
+	</div>
+</div>
--- svelte-ui/src/routes/index.svelte
+++ svelte-ui/src/routes/index.svelte
@@ -1,16 +1,7 @@
-<div>
-	<div
-		class="flex flex-wrap border-solid border-2 overflow-clip shadow-xl  rounded-md  md:w-1/2">
-		<div >
-			<img src="garage-buddy-project.png" alt="Profile picture" />
-			<div class="p-2">
-				<p class="text-2xl font-semibold ">Garage buddy</p>
-				<p class="mt-2">
-					Eine Webanwendung zum Verwalten von Fahrzeug-Inspektionen. Demnächst anstehende Inspektionen werden hervorgehoben.
-                    Es war ein Hobby-Projekt um mehr über React, Next.js und Firebase zu lernen.
-                    <!-- Link zum Blog-Post -->
-				</p>
-			</div>
-		</div>
-	</div>
+<script>
+	import Project from '../components/project.svelte';
+</script>
+
+<div class="flex flex-wrap justify-center">
+	<Project />
 </div>
