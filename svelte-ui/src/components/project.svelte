c5c9c73864083975393a3a21b710d5bb8e4f0367
georg-braun <Georg.Braun92@gmail.com>
2021-12-26 21:10:13 +0100

    inject project props

--- svelte-ui/src/components/project.svelte
+++ svelte-ui/src/components/project.svelte
@@ -1,12 +1,16 @@
+<script>
+	export let title = 'Projekt';
+	export let description = 'Projektbeschreibung';
+	export let imagePath = 'default-project-image.png';
+</script>
+
 <div class="border-solid border-2 overflow-clip shadow-xl  rounded-md  md:w-1/3 m-4">
 	<div>
-		<img src="garage-buddy-project.png" alt="Profile picture" />
+		<img src={imagePath} alt="Profile picture" />
 		<div class="p-2">
-			<p class="text-2xl font-semibold ">Garage buddy</p>
+			<p class="text-2xl font-semibold ">{title}</p>
 			<p class="mt-2">
-				Eine Webanwendung zum Verwalten von Fahrzeug-Inspektionen. Demnächst anstehende Inspektionen
-				werden hervorgehoben. Es war ein Hobby-Projekt um mehr über React, Next.js und Firebase zu
-				lernen.
+				{description}
 				<!-- Link zum Blog-Post -->
 			</p>
 		</div>
--- svelte-ui/src/routes/index.svelte
+++ svelte-ui/src/routes/index.svelte
@@ -3,5 +3,11 @@
 </script>
 
 <div class="flex flex-wrap justify-center">
-	<Project />
+	<Project
+		title="Garage Buddy"
+		imagePath="garage-buddy-project.png"
+		description="Eine Webanwendung zum Verwalten von Fahrzeug-Inspektionen. Demnächst anstehende Inspektionen
+	werden hervorgehoben. Es war ein Hobby-Projekt um mehr über React, Next.js und Firebase zu
+	lernen."
+	/>
 </div>
