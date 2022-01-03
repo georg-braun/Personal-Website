7e3e4dc493198c5a46921f2ca3fa1f7f54b2a0f7
georg-braun <Georg.Braun92@gmail.com>
2022-01-03 19:01:46 +0100

    get layout path

--- svelte-ui/svelte.config.js
+++ svelte-ui/svelte.config.js
@@ -3,15 +3,14 @@ import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import { join } from 'path';
 
+const blog_layout = join(process.cwd(), '/src/layout/blog-post.svelte');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess(),
-		mdsvex({ extensions: ['.md'], layout: './src/layout/blog-post.svelte' })
-	],
+	preprocess: [preprocess(), mdsvex({ extensions: ['.md'], layout: blog_layout })],
 
 	kit: {
 		adapter: adapter(),
