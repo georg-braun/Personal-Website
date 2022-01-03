61427a518c04453f6faae41d0e75b4b8f21ed4ff
georg-braun <Georg.Braun92@gmail.com>
2022-01-03 18:37:41 +0100

    add layout for blog posts

---
+++ svelte-ui/src/layout/blog-post.svelte
@@ -0,0 +1,3 @@
+<div class="container mx-auto md:w-4/5">
+	<slot />
+</div>
--- svelte-ui/svelte.config.js
+++ svelte-ui/svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import { join } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".svelte", ".md"],
+	extensions: ['.svelte', '.md'],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), mdsvex({ extensions: ['.md'] })],
+	preprocess: [
+		preprocess(),
+		mdsvex({ extensions: ['.md'], layout: './src/layout/blog-post.svelte' })
+	],
 
 	kit: {
 		adapter: adapter(),
