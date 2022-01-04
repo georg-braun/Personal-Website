701fe1c9bfe4e612ac5cdb0cb7f5b53ee58fb0aa
georg-braun <Georg.Braun92@gmail.com>
2022-01-04 20:39:53 +0100

    post header

--- svelte-ui/src/blog-post.svelte
+++ svelte-ui/src/blog-post.svelte
@@ -1,3 +1,13 @@
+<script>
+	export let title = 'Default title';
+	export let date = "";
+</script>
+
 <div class="container mx-auto md:w-4/5">
+	<div class="mb-8">
+		<h1 class="text-4xl">{title}</h1>
+		<p>{date}</p>
+	</div>
+	
 	<slot />
 </div>
