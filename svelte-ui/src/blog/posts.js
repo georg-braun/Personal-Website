6f62d5feee9bfdd3384dc067382c1be313747b27
georg-braun <Georg.Braun92@gmail.com>
2022-01-11 20:31:49 +0100

    refactor: posts functions

--- svelte-ui/src/blog/posts.js
+++ svelte-ui/src/blog/posts.js
@@ -1,4 +1,42 @@
+/**
+ * Functions to get the posts metadata from the markdown files
+ */
+
+export function getPostsDescByDate() {
+	const posts = getAllPosts();
+	const sortedPosts = sortPostsByDateDesc(posts);
+	return sortedPosts;
+}
+
+export function getLatestFivePostsDescByDate() {
+	const allPostsDescByDate = getPostsDescByDate();
+	return allPostsDescByDate.slice(0, 5);
+}
+
+let postsCache;
+function getAllPosts() {
+	const markdownFiles = getMarkdownFiles();
+	if (!postsCache) postsCache = getPosts(markdownFiles);
+	return postsCache;
+}
+
+function getMarkdownFiles() {
+	return import.meta.globEager(`../routes/blog/**/*.md`);
+}
+
+function getPosts(markdownFiles) {
+	// many thanks to https://blog.stranianelli.com/sveltekit-routing-from-other-folders-english/
+	const posts = [];
+	for (let path in markdownFiles) {
+		const rawPost = markdownFiles[path];
+		const post = extractPostData(path, rawPost);
+		posts.push(post);
+	}
+	return posts;
+}
+
 function extractRoute(path) {
+	// remove folder info and markdown file extension => route
 	return path.replace('../routes', '').replace('.md', '');
 }
 
@@ -11,33 +49,12 @@ function extractPostData(rawPath, rawPost) {
 	};
 }
 
-export function getPostsDescByDate() {
-	const allPosts = import.meta.globEager(`../routes/blog/**/*.md`);
-
-	// many thanks to https://blog.stranianelli.com/sveltekit-routing-from-other-folders-english/
-	const posts = [];
-	for (let path in allPosts) {
-		const rawPost = allPosts[path];
-		const post = extractPostData(path, rawPost);
-		posts.push(post);
-	}
-
-	console.log(posts);
-
-	posts.sort((a, b) => {
-		return new Date(b.metadata.date).valueOf() - new Date(a.metadata.date).valueOf();
-	});
-
-	console.log(posts);
+function sortPostsByDateDesc(postsToSort) {
+	const posts = [...postsToSort];
+	posts.sort(compareFuncDateDesc);
 	return posts;
 }
 
-export function getLatestFivePostsDescByDate() {
-	const allPostsDescByDate = getPostsDescByDate();
-	const latestFivePostsDescByDate = [];
-	for (let postIndex = 0; postIndex < 5; postIndex++) {
-		const post = allPostsDescByDate[postIndex];
-		if (post) latestFivePostsDescByDate.push(post);
-	}
-	return latestFivePostsDescByDate;
+function compareFuncDateDesc(a, b) {
+	return new Date(b.metadata.date).valueOf() - new Date(a.metadata.date).valueOf();
 }
