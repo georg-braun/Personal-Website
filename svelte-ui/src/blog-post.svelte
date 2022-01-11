2ac429ce2b0f45c9d7c7ecaa4c03c8e876463fd0
georg-braun <Georg.Braun92@gmail.com>
2022-01-11 18:48:01 +0100

    format date

--- svelte-ui/src/blog-post.svelte
+++ svelte-ui/src/blog-post.svelte
@@ -1,13 +1,13 @@
 <script>
 	export let title = 'Default title';
-	export let date = "";
+	export let date = '';
 </script>
 
 <div class="container mx-auto md:w-4/5">
 	<div class="mb-8">
 		<h1 class="text-4xl">{title}</h1>
-		<p>{date}</p>
+		<p>{new Date(date).toLocaleDateString()}</p>
 	</div>
-	
+
 	<slot />
 </div>
