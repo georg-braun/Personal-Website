a7af6ecd6241ae8a527cc413f9668523e5e064aa
georg-braun <Georg.Braun92@gmail.com>
2022-01-11 19:58:18 +0100

    style blog posts

--- svelte-ui/src/blog-post.svelte
+++ svelte-ui/src/blog-post.svelte
@@ -1,13 +1,13 @@
 <script>
+	import './blog-post.css';
 	export let title = 'Default title';
 	export let date = '';
 </script>
 
-<div class="container mx-auto md:w-4/5">
+<div class="blog-post container mx-auto md:w-4/5">
 	<div class="mb-8">
 		<h1 class="text-4xl">{title}</h1>
 		<p>{new Date(date).toLocaleDateString()}</p>
 	</div>
-
 	<slot />
 </div>
