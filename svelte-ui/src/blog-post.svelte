4379052467bbfa3e6865abe019f0acdaf69399a4
georg-braun <Georg.Braun92@gmail.com>
2022-01-13 07:44:48 +0100

    bug: double class because of h1 css

--- svelte-ui/src/blog-post.svelte
+++ svelte-ui/src/blog-post.svelte
@@ -6,7 +6,7 @@
 
 <div class="blog-post container mx-auto md:w-4/5">
 	<div class="mb-8">
-		<h1 class="text-4xl">{title}</h1>
+		<p class="text-4xl">{title}</p>
 		<p>{new Date(date).toLocaleDateString()}</p>
 	</div>
 	<slot />
