49ddebdaaffc2c2a603746dbb894ec15ee4af269
Georg Braun <5886884+georg-braun@users.noreply.github.com>
2022-01-25 13:34:00 +0100

    Update about.svelte

--- svelte-ui/src/routes/about.svelte
+++ svelte-ui/src/routes/about.svelte
@@ -2,26 +2,16 @@
 	<h1 class="text-xl">About me</h1>
 
 	<p class="mt-4">
-		My name is Georg Braun and live in Germany (Simmerath). My current day job is working as a C#
+		My name is Georg Braun and live in Germany. My current day job is working as a C#
 		software engineer. In my spare time, I'm interested in checking out more about C#, architecture,
 		web development, DevOps.
 	</p>
 	<p class="mt-4">
-		My blog is about projects, opinions, problems I encounter as a software engineer. I think
-		blogging has several advantages: reflection, learning by teaching, offer hopefully some useful
-		information, getting feedback.
+		My blog is about projects, opinions, and problems I encounter as a software engineer. I think
+		blogging has several advantages: reflection, learning by teaching, and offer hopefully some useful
+		information
 	</p>
-
-	<h1 class="mt-4 text-xl">My stages in software engineering</h1>
-	<p class="mt-4">
-		At the age of about 13, my interest in computer science started. At first, it was about playing
-		video games and creating simple websites.
-	</p>
-	<p class="mt-4">
-		Here is a shortlist of stages: Apprentice ship software engineering, start working at INFORM
-		GmbH, bachelor degree computer sciences with internship bosch engineering, master degree
-		information systems engineering, day job C# software engineering at INFORM GmbH.
-	</p>
+	
 	<p class="mt-4">
 		If you want to see a full list of stages visit my <a
 			target="_blank"
