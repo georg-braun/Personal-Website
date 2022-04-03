2b60ed96bae433c32d3e56574eb773c7f9ff97c9
Georg <Georg.Braun92@gmail.com>
2022-04-03 16:09:07 +0200

    new project list

--- svelte-ui/src/components/project-template.svelte
+++ svelte-ui/src/components/project-template.svelte
@@ -12,30 +12,29 @@
 	export let tags = [];
 </script>
 
-<div class="border-solid border-2 overflow-clip shadow-xl  rounded-md w-full md:w-1/3 m-8">
+<div class="border-solid border-2 overflow-clip shadow-xl rounded-md w-full md:w-2/3 m-8">
 	<div>
-		<img src={imagePath} class="max-h-80 mx-auto" alt="Profile" />
+		<div class="p-2 flex md:flex-nowrap flex-wrap-reverse">
+			<div class="w-fit md:w-1/3 mx-auto">
+				<img src={imagePath} class="mx-auto" alt="Profile" />
 
-		<div class="p-2">
-			<p class="text-xl font-semibold ">{title}</p>
-			<div class="align-middle">
-				<span class="align-middle">{date}</span>
-				<a target="_blank" rel="noopener noreferrer" href={repoUrl}
-					><Icon class="inline align-middle text-xl" icon="akar-icons:github-fill" /></a
-				>
-				{#if projectUrl != ''}
-					<a target="_blank" rel="noopener noreferrer" href={projectUrl}>
-						<Icon class="inline align-middle text-xl" icon="ci:external-link" />
-					</a>
-				{/if}
 			</div>
-			<p class="mt-2">
-				{description}
-			</p>
-			<div class="flex mt-4">
-				{#each tags as tag}
-					<Tag tagName={tag} />
-				{/each}
+			<div class="w-2/3 ml-2">
+				<p class="text-xl font-semibold ">{title}</p>
+				<div class="align-middle">
+					<span class="align-middle">{date}</span>
+					<a target="_blank" rel="noopener noreferrer" href={repoUrl}
+						><Icon class="inline align-middle text-xl" icon="akar-icons:github-fill" /></a
+					>
+					{#if projectUrl != ''}
+						<a target="_blank" rel="noopener noreferrer" href={projectUrl}>
+							<Icon class="inline align-middle text-xl" icon="ci:external-link" />
+						</a>
+					{/if}
+				</div>
+				<div >
+					{description}
+				</div>
 			</div>
 		</div>
 	</div>
