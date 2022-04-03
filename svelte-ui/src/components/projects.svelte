1270fc2ee4818d02b79fde19fd8b283fa549c512
Georg <Georg.Braun92@gmail.com>
2022-04-03 18:54:12 +0200

    filter software projects

--- svelte-ui/src/components/projects.svelte
+++ svelte-ui/src/components/projects.svelte
@@ -1,20 +1,25 @@
 <script>
 	import Project from '../components/project-template.svelte';
 	import Projects from './projects.json';
-	console.log(Projects);
+	let showOnlySoftwareProjects;
 </script>
 
-<div class="flex flex-wrap justify-center">
-{#each Projects as project}
-	<Project
-		title={project.title}
-		imagePath={project.imagePath}
-		repoUrl={project.repoUrl}
-		projectUrl={project.projectUrl}
-		date={project.date}
-		description={project.description}
-	/>
-{/each}
-	
+<div>
+	<input type="checkbox" bind:checked={showOnlySoftwareProjects} class="rounded-ful" />
+	Show only software stuff
+</div>
 
+<div class="flex flex-wrap ">
+	{#each Projects as project}
+		{#if !showOnlySoftwareProjects || project.category === 'it'}
+			<Project
+				title={project.title}
+				imagePath={project.imagePath}
+				repoUrl={project.repoUrl}
+				projectUrl={project.projectUrl}
+				date={project.date}
+				description={project.description}
+			/>
+		{/if}
+	{/each}
 </div>
