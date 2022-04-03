415f52a2e9909afcf30c7e816f8a3f64e9aafb91
Georg <Georg.Braun92@gmail.com>
2022-04-03 19:28:24 +0200

    personal stuff

--- svelte-ui/src/routes/about.svelte
+++ svelte-ui/src/routes/about.svelte
@@ -1,24 +1,38 @@
+<script>
+	export let rockyElmoImage = 'rocky-elmo.png';
+</script>
+
 <div class="w-2/3 mx-auto">
 	<h1 class="text-xl">About me</h1>
 
+	<p class="mt-4">My name is Georg Braun and work as (C#) software engineer in Germany. <br>
+		I'm interessted and evolve in software- and system-architecture, web development and DevOps.</p>
 	<p class="mt-4">
-		My name is Georg Braun and live in Germany. My current day job is working as a C#
-		software engineer. In my spare time, I'm interested in checking out more about C#, architecture,
-		web development, DevOps.
+		<br />
+		My spare time I mostly spent with
 	</p>
+	<ul>
+		<li>my girlfriend</li>
+		<li>these two guys <img src={rockyElmoImage} alt="animals" /></li>
+		<li>solving problems / do helpful stuff with software</li>
+		<li>bicycles</li>
+		<li>motorcycles</li>
+		<li>build stuff</li>
+	</ul>
 	<p class="mt-4">
 		My blog is about projects, opinions, and problems I encounter as a software engineer. I think
-		blogging has several advantages: reflection, learning by teaching, and offer hopefully some useful
-		information
+		blogging has several advantages: reflection, learning by teaching, and offer hopefully some
+		useful information.<br>
+		Maybe there will sneak in some posts about my other interests ;)
 	</p>
-	
+
 	<p class="mt-4">
-		If you want to see a full list of stages visit my <a
+		If you want to see a full list of stages and buzzwords visit my <a
 			target="_blank"
 			rel="noopener noreferrer"
 			href="https://www.linkedin.com/in/georg-braun-41286b140/">LinkedIn profile</a
 		>
-		or contact me <a href="mailto:mail@georg-braun.de">mail@georg-braun.de</a> :)
+		or contact me mail.at.georg-braun.de :)
 	</p>
 </div>
 
