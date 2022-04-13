0f6eeed3cf8ef163907b490a7acba0ace3e8c8d1
Georg <Georg.Braun92@gmail.com>
2022-04-13 17:32:10 +0200

    improvements ;)

--- svelte-ui/src/routes/__layout.svelte
+++ svelte-ui/src/routes/__layout.svelte
@@ -13,15 +13,14 @@
 						<img src="/avatar.png" alt="Profile" />
 					</div>
 				</div>
-				<div class="w-full sm:w-auto mt-2 sm:mt-0 font-mono text-sm text-gray-400">
+				<div class="w-full sm:w-auto mt-2 sm:my-auto font-mono text-sm text-gray-400">
 					{'{'}<br>
-					<div class="ml-8">title: "Georg Braun",</div>
-					<div class="ml-8">sites: [ 
-						"<a class="underline text-black" href="/">start</a>", 
-						"<a class="underline text-black" href="/blog">blog</a>", 
-						"<a class="underline text-black" href="/projects">projects</a>", 
-						"<a class="underline text-black" href="/about">about</a>" ],</div>
-					<div class="ml-8 ">content: [ "software", "architecture", "C#", "DevOps", "projects", "opinions", "share knowledge"]</div>
+					<div class="ml-8">title: Georg Braun,</div>
+					<div class="ml-8">pages: [<a class="underline text-black" href="/">start</a>,
+						<a class="underline text-black" href="/blog">blog</a>,
+						<a class="underline text-black" href="/projects">projects</a>,
+						<a class="underline text-black" href="/about">about</a>],</div>
+					<div class="ml-8 ">content: [software, backend, DevOps, projects, opinions, share knowledge]</div>
 					{'}'}
 					<p class="text-center">
 						
@@ -29,9 +28,6 @@
 						
 						
 					</p>
-				</div>
-				<div class="mx-auto sm:ml-auto sm:mr-10">
-					<h3>I ❤️🛠️</h3>
 				</div>
 			</div>
 		</div>
