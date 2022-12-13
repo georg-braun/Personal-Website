071b0a734bdb99346bb296331b3a639df946e06e
Georg Braun <5886884+georg-braun@users.noreply.github.com>
2022-12-13 06:25:06 +0000

    remove unnecessary stuff

--- svelte-ui/src/routes/__layout.svelte
+++ svelte-ui/src/routes/__layout.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import '../prism.css';
-
-	let scrollY;
-	let innerWidth;
-	let showBigHeader = true;
-
-	$: {
-		// wait some ms to change the header
-		setTimeout(() => {
-			// avoid that the reszie to the smaller header and the resulting scrollbar position
-			// results again in a big header
-			const heightDiffInPixelBetweenSmallAndBigHeader = 70;
-			//showBigHeader = scrollY <= heightDiffInPixelBetweenSmallAndBigHeader + 100;
-		}, 1000);
-	}
 </script>
-
-<svelte:window bind:scrollY bind:innerWidth />
 
 <main>
 	<div class=" ">
@@ -25,14 +9,11 @@
 			<img src="/avatar.png" alt="Profile" />
 		</div>
 
-		<div class="border border-2" />
-
-		<div class="flex mx-auto justify-center p-2">
+		<div class="flex mx-auto justify-center p-2 border border-x-0 border-y-4">
 			<a href="/"><div>Blog</div></a>
 			<a class="ml-4" href="/stuff"><div>Projects</div> </a>
 			<a class="ml-4" href="/about"><div>About</div></a>
 		</div>
-		<div class="border border-2" />
 	</div>
 
 	<div class="mt-10">
