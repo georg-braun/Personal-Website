7c7072fdaf4c619722a469316fdda415ef9d8afe
Georg <Georg.Braun92@gmail.com>
2023-01-09 21:59:23 +0100

    about me page

---
+++ website/src/lib/components/Header.svelte
@@ -0,0 +1,59 @@
+<div class="image-container">
+	<img src="/avatar.png" alt="Profile" width="200px" />
+</div>
+
+<div class="navigation">
+<div><a href="/" class="navigation__link">
+    <div class="navigation__link__text">Home</div>
+</a></div>
+<div><a href="/projects" class="navigation__link">
+    <div class="navigation__link__text">Projects</div>
+</a></div>
+<div><a href="/aboutme" class="navigation__link">
+    <div class="navigation__link__text">About me</div>
+</a></div>
+</div>
+
+
+<style lang="scss">
+    .image-container{
+        text-align: center;
+        margin-bottom: -4px;
+        padding: 0px;
+    }
+    .line{
+        width: 500px;
+        height: 1px;
+        background-color: black;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .navigation {
+        display: flex;
+        justify-content: center;
+    }
+    .navigation__link__text {
+        &:hover{
+            background: #ECEFF4;
+        }
+        
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-left: 20px;
+        padding-right: 20px;
+
+        font-size: 1.25em;
+        width: 100px;
+
+        text-align: center;
+        border-top: solid;
+        border-bottom: solid;
+        border-color: black;
+    }
+    .navigation__link{
+        text-decoration: none ;
+        color: black;
+
+    }
+</style>
--- website/src/routes/blog/[slug]/+page.svelte
+++ website/src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,23 @@
 <script>
+    import dayjs from "dayjs"
     export let data;
+    console.log(data)
 </script>
 
-<h1>{data.title}</h1>
+<div class="post-title">
+    <h2>{data.title}</h2>
+    {dayjs(data.date).format("MMM D, YYYY")}
+</div>
+
 <svelte:component this={data.content} />
+
+<style>
+    .post-title{
+        height: 100px;
+
+        background-color: #E5E9F0;
+
+        text-align: center;
+        padding: 20px;
+    }
+</style>
