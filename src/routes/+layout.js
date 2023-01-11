59397a45d0145332c23bd7f68e4845cb19dde277
Georg <Georg.Braun92@gmail.com>
2023-01-11 10:40:37 +0100

    add dark / light mode

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,7 @@
 /** svelte static adapter */
 export const prerender = true;
-export const csr = false;
+
+/** uncommenct this to deploy the complete website as server side rendered.
+ * This implies that there is no javascript available.
+  */
+// export const csr = false;
