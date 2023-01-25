ae47676d55933db7fb86dfd2f81009247db2f40a
Georg <Georg.Braun92@gmail.com>
2023-01-25 06:44:18 +0100

    update tag color

--- src/lib/utils/tags.js
+++ src/lib/utils/tags.js
@@ -1,10 +1,15 @@
-export function getTagColor(tag){
+export function getTagColor(tag) {
     return colorCodes[tag] ?? "lightgray";
 }
 
-
 const colorCodes = {
+    /** Backend */
     "dotnet": "rgba(75,41,205,0.5)",
     "asp.net": "rgba(75,41,205,0.5)",
+    /** Web frontend */
+    "sveltekit": "rgba(236,96,50,0.5)",
+    /** Database */
+    /** DevOps */
     "docker": "rgba(69,149,203,0.5)",
+    "devops": "rgba(69,149,203,0.5)",
 }
